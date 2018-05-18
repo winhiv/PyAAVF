6b499240129e7ff1d2a2816ab6f79e55871e400d
Matthew Fogel <matthew.fogel@canada.ca>
2018-05-18 10:56:54 -0500

    Modify setup.py and tox.ini to correctly package project

--- setup.py
+++ setup.py
@@ -1,36 +1,25 @@
 from setuptools import setup
 
+from setuptools import find_packages, setup
+
 DEPENDENCIES = ['setuptools']
 
 setup(
     name='PyAAVF',
     version='0.1.0',
-    packages=('PyAAVF',),
-    url='https://github.com/winhiv/PyAAVF',
-    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    url='https://github.com/winhiv/PyAAVF.git',
+    license='Apache License, Version 2.0',
     author='Matthew Fogel',
     author_email='matthew.fogel@canada.ca',
     install_requires=DEPENDENCIES,
     tests_require=(),
     description='An Amino Acid Variant Format parser for Python.',
-    entry_points=(
-        'PyAAVF.filters': [
-            'site_quality = PyAAVF.filters:SiteQuality',
-            'vgq = PyAAVF.filters:AAVariantGenotypeQuality',
-            'eb = PyAAVF.filters:ErrorBiasFilter',
-            'dps = PyAAVF.filters:DepthPerSample',
-            'avg-dps = PyAAVF.filters:AvgDepthPerSample',
-            'snp-only = PyAAVF.filters.SnpOnly',
-        ]
-    )
+    entry_points='',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy')]
+    ]
+)
