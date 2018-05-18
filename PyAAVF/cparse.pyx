c62589842c00c9b7d78ca7b80902cdb914b3a1b2
Matthew Fogel <matthew.fogel@canada.ca>
2018-05-18 08:57:53 -0500

    Modify LICENSE and notices

---
+++ PyAAVF/cparse.pyx
@@ -0,0 +1,23 @@
+"""
+Copyright Government of Canada 2018
+
+Modified by: Matthew Fogel, National Microbiology Laboratory,
+             Public Health Agency of Canada
+
+Originally written by: Population Genetics Technologies Ltd,
+                       Copyright (c) 2011-2012
+
+Originally written by: John Dougherty,
+                       Copyright (c) 2011
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this work except in compliance with the License. You may obtain a copy of the
+License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
