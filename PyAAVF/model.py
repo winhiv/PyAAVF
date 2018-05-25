c1c3d05b4fd042d12e9196d83b4408f1dd47d868
Matthew Fogel <matthew.fogel@canada.ca>
2018-05-25 14:48:24 -0500

    Complete adding utilities to walk through several readers at once and tests for equality

--- PyAAVF/model.py
+++ PyAAVF/model.py
@@ -47,26 +47,50 @@ class _Record(object):
         """__cmp___"""
         return cmp((self.CHROM, self.POS),
                    (getattr(other, "CHROM", None),
+                    getattr(other, "GENE", None),
                     getattr(other, "POS", None)))
 
     # For Python 3
     def __eq__(self, other):
-        """ _Records are equal if they describe the same variant (same position, amino acids) """
-        return (self.CHROM == getattr(other, "CHROM", None) and
-                self.POS == getattr(other, "POS", None) and
-                self.REF == getattr(other, "REF", None) and
-                self.ALT == getattr(other, "ALT", None))
+        """ _Records are equal if they describe the same variant (same position, amino acids).
+            If AC is present in the INFO column of both _Records then INFO[AC] must be equal
+            for _Records to be equal.
+        """
+        ac_present = False
+        if "AC" in self.INFO and "AC" not in other.INFO:
+            return False
+        if "AC" not in self.INFO and "AC" in other.INFO:
+            return False
+
+        if "AC" in self.INFO and "AC" in other.INFO:
+            ac_present = True  # AC is present in both INFO fields
+
+        equal_rec = (self.CHROM == getattr(other, "CHROM", None) and
+                     self.GENE == getattr(other, "GENE", None) and
+                     self.POS == getattr(other, "POS", None) and
+                     self.REF == getattr(other, "REF", None) and
+                     self.ALT == getattr(other, "ALT", None))
+
+        if ac_present:
+            equal_rec = equal_rec and \
+                        (self.INFO["AC"] == getattr(other, "INFO", None).get("AC", None))
+
+        return equal_rec
 
     # For Python 3
     def __lt__(self, other):
         """__lt__"""
-        lhs = (self.CHROM, self.POS)
-        rhs = (getattr(other, "CHROM", None), getattr(other, "POS", None))
+        lhs = (self.CHROM, self.GENE, self.POS)
+        rhs = (getattr(other, "CHROM", None),
+               getattr(other, "GENE", None),
+               getattr(other, "POS", None))
         return lhs < rhs
 
     def __str__(self):
         """str"""
-        return "Record(CHROM=%(CHROM)s, GENE=%(GENE)s, POS=%(POS)s, REF=%(REF)s" % self.__dict__
+        return (("Record(CHROM=%(CHROM)s, GENE=%(GENE)s, POS=%(POS)s, REF=%(REF)s, " +
+                 "ALT=%(ALT)s, FILTER=%(FILTER)s, ALT_FREQ=%(ALT_FREQ)s, " +
+                 "COVERAGE=%(COVERAGE)s, INFO=%(INFO)s)") % self.__dict__)
 
     def add_filter(self, flt):
         """add filter"""
@@ -78,3 +102,12 @@ class _Record(object):
     def add_info(self, info, value=True):
         """add_info"""
         self.INFO[info] = value
+
+    @property
+    def is_filtered(self):
+        """ Return True if a variant has been filtered """
+        filt = self.FILTER
+        if filt is None or not filt:  # FILTER is not set or set to PASS
+            return False
+        else:
+            return True
